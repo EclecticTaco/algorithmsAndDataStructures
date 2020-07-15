7ff1905b0f0e78301999c9ca67f3b50bd1206b69
eclectictaco <calvin5210@gmail.com>
2020-07-15 16:02:42 -0700

    add get func

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -65,7 +65,27 @@ class DoublyLinkedList {
       this.head = newNode;
     }
     this.length += 1;
-    return this
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count, current;
+    if (index <= this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count += 1;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count -= 1;
+      }
+    }
+    return current;
   }
 }
 
