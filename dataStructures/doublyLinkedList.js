e9e7173cb069047bf8a855f789b545ff9268ba56
eclectictaco <calvin5210@gmail.com>
2020-07-15 15:38:39 -0700

    add unshift func

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -54,4 +54,18 @@ class DoublyLinkedList {
     }
     return oldHead;
   }
+  unShift(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length += 1;
+    return this
+  }
 }
+
