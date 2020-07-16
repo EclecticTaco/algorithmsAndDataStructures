50d1be2e1c1ba8b641a410b2e0a5433b493c82cc
eclectictaco <calvin5210@gmail.com>
2020-07-16 10:57:32 -0700

    add push func

---
+++ dataStructures/stack.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    let node = new Node(val);
+    if (this.size === 0) {
+      this.first = node;
+      this.last = node;
+    } else {
+      let oldFirst = this.first;
+      this.first = node;
+      node.next = oldFirst;
+    }
+    return this.size += 1;
+  }
+}
+
